59242a1eadd7e1e4576bf02f6533fd7ee472621b
ramazan <ramazan.aksoy1907@hotmail.com>
2022-11-26 18:26:28 +0300

    shimmer animation add

--- lib/views/favorite/view/favorite.dart
+++ lib/views/favorite/view/favorite.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:spotify_api/core/base/base_loading.dart';
 import 'package:spotify_api/utils/extensions/time_converter.dart';
 import 'package:spotify_api/views/favorite/view-model/favorities_view_model.dart';
 
@@ -44,126 +45,121 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
 
   Widget artistSongsList() {
     return Consumer(
-      builder: (context, FavoritiesViewModel value, child) => value
-              .isLoadingArtistTopTrack
-          ? Container()
-          : Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 26, left: 3.6.h),
-                  width: 345,
-                  height: 20,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          child: Text("Songs",
-                              style: GoogleFonts.roboto(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 16,
-                                  color: const Color(0xff222222)))),
-                      Text("See more",
-                          style: GoogleFonts.roboto(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 11,
-                              color: const Color(0xff131313))),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 370,
-                  child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: value.artistTopTracks!.tracks!.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        //color: Colors.red,
-                        margin: EdgeInsets.only(bottom: 2.5.h, left: 3.6.h),
-                        height: 6.h,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: 21.h,
-                              height: 6.h,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    alignment: Alignment.center,
-                                    width: 5.h,
-                                    height: 5.h,
-                                    decoration: const BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Color(0xffE6E6E6)),
-                                    child: Image.asset("assets/play.png"),
-                                  ),
-                                  Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      SingleChildScrollView(
-                                        child: SizedBox(
-                                          width: 30.w,
-                                          height: 2.h,
-                                          child: Text(
-                                              value.artistTopTracks!
-                                                  .tracks![index].album!.name
-                                                  .toString(),
-                                              style: GoogleFonts.roboto(
-                                                  fontWeight: FontWeight.w700,
-                                                  fontSize: 16.sp,
-                                                  color:
-                                                      const Color(0xff000000))),
-                                        ),
-                                      ),
-                                      Text(
-                                          value.artistTopTracks!.tracks![index]
-                                              .album!.artists![0].name
-                                              .toString(),
-                                          style: GoogleFonts.roboto(
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 1.3.h,
-                                              color: const Color(0xff000000)))
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(
-                              width: 12.h,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                      TimeConverter()
-                                          .milisecontToSecondAndMinute(value
-                                              .artistTopTracks!
-                                              .tracks![index]
-                                              .durationMs),
+        builder: (context, FavoritiesViewModel value, child) =>
+        value.isLoadingArtistTopTrack?Container():artistSongListCurrentWidget(value));
+
+  }
+
+
+  Column artistSongListCurrentWidget(FavoritiesViewModel value) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 26, left: 3.6.h),
+          width: 345,
+          height: 20,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  child: Text("Songs",
+                      style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          color: const Color(0xff222222)))),
+              Text("See more",
+                  style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 11,
+                      color: const Color(0xff131313))),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 370,
+          child: ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: value.artistTopTracks!.tracks!.length,
+            itemBuilder: (context, index) {
+              return Container(
+                //color: Colors.red,
+                margin: EdgeInsets.only(bottom: 2.5.h, left: 3.6.h),
+                height: 6.h,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 21.h,
+                      height: 6.h,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            width: 5.h,
+                            height: 5.h,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xffE6E6E6)),
+                            child: Image.asset("assets/play.png"),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SingleChildScrollView(
+                                child: SizedBox(
+                                  width: 30.w,
+                                  height: 2.h,
+                                  child: Text(
+                                      value.artistTopTracks!.tracks![index]
+                                          .album!.name
+                                          .toString(),
                                       style: GoogleFonts.roboto(
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 1.9.h,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16.sp,
                                           color: const Color(0xff000000))),
-                                  Image.asset("assets/heart.png"),
-                                ],
+                                ),
                               ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                )
-              ],
-            ),
+                              Text(
+                                  value.artistTopTracks!.tracks![index].album!
+                                      .artists![0].name
+                                      .toString(),
+                                  style: GoogleFonts.roboto(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 1.3.h,
+                                      color: const Color(0xff000000)))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12.h,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                              TimeConverter().milisecontToSecondAndMinute(value
+                                  .artistTopTracks!.tracks![index].durationMs),
+                              style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 1.9.h,
+                                  color: const Color(0xff000000))),
+                          Image.asset("assets/heart.png"),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 
